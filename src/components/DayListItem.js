1862f6a6335c1d15a58c931b3657ad0c12fdade7
Miguel Jimenez <migang9@gmail.com>
2021-08-03 23:48:46 -0400

    Styling the DayListi

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,28 @@
 import React from "react";
 
+import "components/DayListItem.scss";
+
+import classnames from "classnames";
+
 export default function DayListItem(props) {
+  function formalSpots() {
+    if(props.spots === 0) {
+      return "no spots remaining"
+    } else if (props.spots === 1) {
+      return "1 spot remaining"
+    } else {
+      return `${props.spots} spots remaining`
+    }
+  }
+
+  const dayClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
   return (
-    <li onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots}</h3>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formalSpots()}</h3>
     </li>
   );
 }
