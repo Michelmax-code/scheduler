cb6ffe2257750d6257e0b95d4eaf7577d46e5405
Miguel Jimenez <migang9@gmail.com>
2021-08-09 23:43:19 -0400

    First test, queries, matchers, firing events, test driven nd development steps in form.test and form.js

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -7,7 +7,14 @@ import InterviewerList from "components/InterviewerList";
 const Form = function(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
-
+  const [error, setError] = useState("");
+  function validate() {
+    if(name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    props.onSave(name, interviewer);
+  }
   function reset() {
     setName("");
     setInterviewer(null);
@@ -31,9 +38,11 @@ const Form = function(props) {
           This must be a controlled component
         */
             onChange={(event) => setName(event.target.value)}
+            data-testid="student-name-input"
           />
-          {name}
+          
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList
           interviewers={props.interviewers}
           interviewer={interviewer}
@@ -43,7 +52,7 @@ const Form = function(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button onClick={cancel} danger>Cancel</Button>
-          <Button onClick={() => props.onSave(name, interviewer)} confirm>Save</Button>
+          <Button onClick={validate} confirm>Save</Button>
         </section>
       </section>
     </main>
